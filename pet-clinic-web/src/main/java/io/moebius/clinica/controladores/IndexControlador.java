e54ad7062a9e88da002eb0c6427446f5d7e858b8
Álvaro González Llorente <alvaro.gonzalez@grupocmc.es>
2019-02-25 13:51:38 +0100

    Index page and controller. Closes #4

---
+++ pet-clinic-web/src/main/java/io/moebius/clinica/controladores/IndexControlador.java
@@ -0,0 +1,14 @@
+package io.moebius.clinica.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexControlador {
+	
+	@RequestMapping({"", "/", "index", "index.html"})
+	public String index() {
+		return "index";
+	}
+}
