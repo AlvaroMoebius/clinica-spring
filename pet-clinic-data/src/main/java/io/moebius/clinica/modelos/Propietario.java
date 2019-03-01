0d374a82955f5f2d6bcba8bd278dd0b31b14e29d
Álvaro González Llorente <avr.gzz@gmail.com>
2019-03-01 12:12:25 +0100

    Propeitario ahora tiene mascotas. Creada Visita. Close # 22

--- pet-clinic-data/src/main/java/io/moebius/clinica/modelos/Propietario.java
+++ pet-clinic-data/src/main/java/io/moebius/clinica/modelos/Propietario.java
@@ -1,4 +1,22 @@
 package io.moebius.clinica.modelos;
 
+import java.util.Set;
+
 public class Propietario extends Persona {
+	
+	private Set<Mascota> mascotas;
+
+	
+	
+	
+	public Set<Mascota> getMascotas() {
+		return mascotas;
+	}
+
+	public void setMascotas(Set<Mascota> mascotas) {
+		this.mascotas = mascotas;
+	}
+	
+	
+	
 }
---
+++ pet-clinic-data/src/main/java/io/moebius/clinica/modelos/Visita.java
@@ -0,0 +1,33 @@
+package io.moebius.clinica.modelos;
+
+import java.time.LocalDate;
+
+public class Visita extends EntidadBase{
+
+	private LocalDate fecha;
+	private String descripcion;
+	private Mascota mascota;
+	
+	
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Mascota getMascota() {
+		return mascota;
+	}
+	public void setMascota(Mascota mascota) {
+		this.mascota = mascota;
+	}
+	
+	
+	
+}
