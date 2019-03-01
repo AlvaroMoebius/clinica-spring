3ca3d22a1de96df1c1afe7e2e62374347f44a26f
Álvaro González Llorente <alvaro.gonzalez@grupocmc.es>
2019-03-01 12:23:50 +0100

    Añadida información de cotnacto de propietario. Close #24

--- pet-clinic-data/src/main/java/io/moebius/clinica/modelos/Propietario.java
+++ pet-clinic-data/src/main/java/io/moebius/clinica/modelos/Propietario.java
@@ -4,11 +4,37 @@ import java.util.Set;
 
 public class Propietario extends Persona {
 	
+	private String direccion;
+	private String ciudad;
+	private String telefono;
+	
 	private Set<Mascota> mascotas;
+	
+	// GETTERS & SETTERS
+	public String getDireccion() {
+		return direccion;
+	}
 
-	
-	
-	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
 	public Set<Mascota> getMascotas() {
 		return mascotas;
 	}
@@ -16,6 +42,7 @@ public class Propietario extends Persona {
 	public void setMascotas(Set<Mascota> mascotas) {
 		this.mascotas = mascotas;
 	}
+
 	
 	
 	
