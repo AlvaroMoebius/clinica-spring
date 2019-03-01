03fc61e718f4679d4ff297a812b0e637c9cf2486
Álvaro González Llorente <avr.gzz@gmail.com>
2019-03-01 14:09:04 +0100

    Añadir contatos

--- pet-clinic-web/src/main/java/io/moebius/clinica/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/io/moebius/clinica/bootstrap/DataLoader.java
@@ -28,10 +28,41 @@ public class DataLoader implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+
+		// Tipos de mascota
+		TipoMascota gato = new TipoMascota();
+		gato.setNombre("Gato");
+		
+		TipoMascota guardadoGatoTipoMascota = tipoMascotaServicio.save(gato);
+		
+		TipoMascota loro = new TipoMascota();
+		gato.setNombre("Loro");
+		
+		TipoMascota guardadoLoroTipoMascota = tipoMascotaServicio.save(loro);
+		
+		
+		TipoMascota perro = new TipoMascota();
+		gato.setNombre("Perro");
+		
+		TipoMascota guardadoPerroTipoMascota = tipoMascotaServicio.save(perro);
+		
+		
+		TipoMascota canguro = new TipoMascota();
+		gato.setNombre("Canguro");
+		
+		TipoMascota guardadoCanguroTipoMascota = tipoMascotaServicio.save(canguro);
+		
+		System.out.println("[DATA LOADER] --INFO-- Cargados tipos de mascota.");
+		
+		
+		
 		// Propietarios
 		Propietario p1 = new Propietario();
 		p1.setNombre("Álvaro");
 		p1.setApellido("González");
+		p1.setDireccion("C/ Pinos");
+		p1.setCiudad("Alcorcón");
+		p1.setTelefono("666999666");
 		
 		propietarioServicio.save(p1);
 		
@@ -39,6 +70,9 @@ public class DataLoader implements CommandLineRunner{
 		Propietario p2 = new Propietario();
 		p2.setNombre("Carlos");
 		p2.setApellido("Ramírez");
+		p2.setDireccion("C/ Cuba");
+		p2.setCiudad("Aluche");
+		p2.setTelefono("321321321");
 		
 		propietarioServicio.save(p2);
 		
@@ -46,6 +80,9 @@ public class DataLoader implements CommandLineRunner{
 		Propietario p3 = new Propietario();
 		p3.setNombre("Justo");
 		p3.setApellido("Castaño");
+		p3.setDireccion("C/ Normandía");
+		p3.setCiudad("Leganés");
+		p3.setTelefono("111222333");
 		
 		propietarioServicio.save(p3);
 		
@@ -53,34 +90,13 @@ public class DataLoader implements CommandLineRunner{
 		Propietario p4 = new Propietario();
 		p4.setNombre("Martina");
 		p4.setApellido("Oliva");
+		p4.setDireccion("C/ Whitecastle");
+		p4.setCiudad("Londres");
+		p4.setTelefono("987654321");
 		
 		propietarioServicio.save(p4);
 		
 		System.out.println("[DATA LOADER] --INFO-- Cargados propietarios.");
-		
-		// Tipos de mascota
-		
-		TipoMascota gato = new TipoMascota();
-		gato.setNombre("Gato");
-		
-		tipoMascotaServicio.save(gato);
-		
-		TipoMascota loro = new TipoMascota();
-		gato.setNombre("Loro");
-		
-		tipoMascotaServicio.save(loro);
-		
-		
-		TipoMascota perro = new TipoMascota();
-		gato.setNombre("Perro");
-		
-		tipoMascotaServicio.save(perro);
-		
-		
-		TipoMascota canguro = new TipoMascota();
-		gato.setNombre("Canguro");
-		
-		tipoMascotaServicio.save(canguro);
 		
 		
 		// Veterinarios
