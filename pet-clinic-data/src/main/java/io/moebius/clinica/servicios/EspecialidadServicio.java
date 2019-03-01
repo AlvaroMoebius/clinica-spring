9ff13ef481f5e6b0836a07e7f83414a71ed3212d
Álvaro González Llorente <alvaro.gonzalez@grupocmc.es>
2019-03-01 15:04:48 +0100

    Creados EspecialidadServicio e implementacionMap

---
+++ pet-clinic-data/src/main/java/io/moebius/clinica/servicios/EspecialidadServicio.java
@@ -0,0 +1,7 @@
+package io.moebius.clinica.servicios;
+
+import io.moebius.clinica.modelos.Especialidad;
+
+public interface EspecialidadServicio extends CrudService<Especialidad, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/io/moebius/clinica/servicios/map/EspecialidadServicioMapa.java
@@ -0,0 +1,44 @@
+package io.moebius.clinica.servicios.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import io.moebius.clinica.modelos.Especialidad;
+import io.moebius.clinica.servicios.EspecialidadServicio;
+
+@Service
+public class EspecialidadServicioMapa extends AbstractMapService<Especialidad, Long> implements EspecialidadServicio {
+
+	@Override
+	public Especialidad findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Especialidad save(Especialidad especialidad) {
+		
+		return super.save(especialidad);
+	}
+
+	@Override
+	public void delete(Especialidad especialidad) {
+		super.delete(especialidad);
+		
+	}
+
+	@Override
+	public Set<Especialidad> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	
+	public void deleteById(Long id) {
+		
+		super.deleteById(id);
+	}
+	
+	
+
+}
