cbc0e7920df8257f9b2971680c5853761e29c0e9
Álvaro González Llorente <alvaro.gonzalez@grupocmc.es>
2019-03-01 12:36:08 +0100

    Agregado TipoMascotaServicio y TipoMascotaServicioMapa. Cloase #25

---
+++ pet-clinic-data/src/main/java/io/moebius/clinica/servicios/TipoMascotaServicio.java
@@ -0,0 +1,9 @@
+package io.moebius.clinica.servicios;
+
+import io.moebius.clinica.modelos.TipoMascota;
+
+public interface TipoMascotaServicio extends CrudService<TipoMascota, Long> {
+	
+	
+
+}
---
+++ pet-clinic-data/src/main/java/io/moebius/clinica/servicios/map/TipoMascotaServicioMapa.java
@@ -0,0 +1,38 @@
+package io.moebius.clinica.servicios.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import io.moebius.clinica.modelos.TipoMascota;
+import io.moebius.clinica.servicios.TipoMascotaServicio;
+
+@Service
+public class TipoMascotaServicioMapa extends AbstractMapService<TipoMascota, Long> implements TipoMascotaServicio{
+
+	@Override
+	public TipoMascota save(TipoMascota tipoMascota) {
+		return super.save(tipoMascota);
+	}
+
+	@Override
+	public Set<TipoMascota> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public TipoMascota findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(TipoMascota tipoMascota) {
+		super.delete(tipoMascota);
+	}
+	
+}
