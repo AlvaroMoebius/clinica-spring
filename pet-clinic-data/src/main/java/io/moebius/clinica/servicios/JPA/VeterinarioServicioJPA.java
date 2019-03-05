a7b587cf354f114d1676decd22a750c684828605
Álvaro González Llorente <alvaro.gonzalez@grupocmc.es>
2019-03-05 22:02:42 +0100

    Agregado VeterinarioServicioJPA. Closes #37

---
+++ pet-clinic-data/src/main/java/io/moebius/clinica/servicios/JPA/VeterinarioServicioJPA.java
@@ -0,0 +1,50 @@
+package io.moebius.clinica.servicios.JPA;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import org.springframework.stereotype.Service;
+
+import io.moebius.clinica.modelos.Veterinario;
+import io.moebius.clinica.repositorios.VeterinarioRepositorio;
+import io.moebius.clinica.servicios.VeterinarioServicio;
+
+@Service
+public class VeterinarioServicioJPA implements VeterinarioServicio{
+
+	private static final int HashSet = 0;
+	private final VeterinarioRepositorio veterinarioRepositorio;
+	
+	public VeterinarioServicioJPA(VeterinarioRepositorio veterinarioRepositorio) {
+		this.veterinarioRepositorio = veterinarioRepositorio;
+	}
+
+	
+	@Override
+	public Set<Veterinario> findAll() {
+		Set<Veterinario> veterinarios = new HashSet<>();
+		veterinarioRepositorio.findAll().forEach(veterinarios::add);
+		return veterinarios;
+	}
+
+	@Override
+	public Veterinario findById(Long id) {
+		return veterinarioRepositorio.findById(id).orElse(null);
+	}
+
+	@Override
+	public Veterinario save(Veterinario veterinario) {
+		return veterinarioRepositorio.save(veterinario);
+	}
+
+	@Override
+	public void delete(Veterinario veterinario) {
+		veterinarioRepositorio.delete(veterinario);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		veterinarioRepositorio.deleteById(id);
+	}
+
+}
