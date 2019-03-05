cfe566e4279009b0b41eedad85d257e33661785b
Álvaro González Llorente <alvaro.gonzalez@grupocmc.es>
2019-03-05 12:06:32 +0100

    Añadidas anotaciones a entidad base

--- pet-clinic-data/src/main/java/io/moebius/clinica/modelos/EntidadBase.java
+++ pet-clinic-data/src/main/java/io/moebius/clinica/modelos/EntidadBase.java
@@ -2,8 +2,16 @@ package io.moebius.clinica.modelos;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class EntidadBase implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
